b1b524f47590a3d9a0b7447673a477a245a220a0
chathuranga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2023-09-21 15:25:37 +0530

    comment while in get-data.sh

--- get-data.sh
+++ get-data.sh
@@ -9,21 +9,21 @@ set -e
 
 ROOT="open_domain_data"
 
-while [ "$#" -gt 0 ]; do
-    arg=$1
-    case $1 in
-        # convert "--opt=the value" to --opt "the value".
-        # the quotes around the equals sign is to work around a
-        # bug in emacs' syntax parsing
-        --*'='*) shift; set -- "${arg%%=*}" "${arg#*=}" "$@"; continue;;
-        -o|--output) shift; ROOT=$1;;
-        -h|--help) usage; exit 0;;
-        --) shift; break;;
-        -*) usage_fatal "unknown option: '$1'";;
-        *) break;; # reached the list of file names
-    esac
-    shift || usage_fatal "option '${arg}' requires a value"
-done
+# while [ "$#" -gt 0 ]; do
+#     arg=$1
+#     case $1 in
+#         # convert "--opt=the value" to --opt "the value".
+#         # the quotes around the equals sign is to work around a
+#         # bug in emacs' syntax parsing
+#         --*'='*) shift; set -- "${arg%%=*}" "${arg#*=}" "$@"; continue;;
+#         -o|--output) shift; ROOT=$1;;
+#         -h|--help) usage; exit 0;;
+#         --) shift; break;;
+#         -*) usage_fatal "unknown option: '$1'";;
+#         *) break;; # reached the list of file names
+#     esac
+#     shift || usage_fatal "option '${arg}' requires a value"
+# done
 
 DOWNLOAD=$ROOT/"download"
 
